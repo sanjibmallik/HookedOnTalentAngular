ca8f5b76a60e108a3aafd79886787a8213f564a9
Sanjib Mallik <sanjib.mallik@accionlabs.com>
2016-12-23 13:28:57 +0530

    all file added from azhar

---
+++ src/main/webapp/hot/libs/services.js
@@ -0,0 +1,167 @@
+'use strict';
+
+angular.module('xenon.services', []).
+	service('$menuItems', function()
+	{
+		this.menuItems = [];
+
+		var $menuItemsRef = this;
+
+		var menuItemObj = {
+			parent: null,
+
+			title: '',
+			link: '', // starting with "./" will refer to parent link concatenation
+			state: '', // will be generated from link automatically where "/" (forward slashes) are replaced with "."
+			icon: '',
+
+			isActive: false,
+			label: null,
+
+			menuItems: [],
+
+			setLabel: function(label, color, hideWhenCollapsed)
+			{
+				if(typeof hideWhenCollapsed == 'undefined')
+					hideWhenCollapsed = true;
+
+				this.label = {
+					text: label,
+					classname: color,
+					collapsedHide: hideWhenCollapsed
+				};
+
+				return this;
+			},
+
+			addItem: function(title, link, icon)
+			{
+				var parent = this,
+					item = angular.extend(angular.copy(menuItemObj), {
+						parent: parent,
+
+						title: title,
+						link: link,
+						icon: icon
+					});
+
+				if(item.link)
+				{
+					if(item.link.match(/^\./))
+						item.link = parent.link + item.link.substring(1, link.length);
+
+					if(item.link.match(/^-/))
+						item.link = parent.link + '-' + item.link.substring(2, link.length);
+
+					item.state = $menuItemsRef.toStatePath(item.link);
+				}
+
+				this.menuItems.push(item);
+
+				return item;
+			}
+		};
+
+		this.addItem = function(title, link, icon)
+		{
+			var item = angular.extend(angular.copy(menuItemObj), {
+				title: title,
+				link: link,
+				state: this.toStatePath(link),
+				icon: icon
+			});
+
+			this.menuItems.push(item);
+
+			return item;
+		};
+
+		this.getAll = function()
+		{
+			return this.menuItems;
+		};
+
+		this.prepareSidebarMenu = function()
+		{
+			var dashboard    = this.addItem('Dashboard', 		'/app/dashboard', 			'linecons-cog');
+			var users    = this.addItem('Users', 		'/app/users', 			'linecons-cog');
+			var requirments    = this.addItem('Requirments', 		'/app/requirments', 			'linecons-cog');
+			var candidates    = this.addItem('Candidates', 		'/app/candidates', 			'linecons-cog');
+			var questionBank    = this.addItem('Question Bank', 		'/app/questionBank', 			'linecons-cog');
+			var profile    = this.addItem(' My Profile', 		'/app/profile', 			'linecons-cog');
+			var settings    = this.addItem('Settings', 		'/app/settings', 			'linecons-cog');
+			
+
+
+				
+				// Subitems of Users
+				users.addItem('Create User', 	'-/Create-User'); // "-/" will append parents link
+				users.addItem('Display Users', 	'-/Display-Users');
+
+			
+
+
+			return this;
+		};
+
+		this.prepareHorizontalMenu = function()
+		{
+			var dashboard    = this.addItem('Dashboard', 		'/app/dashboard', 			'linecons-cog');
+			
+
+
+				
+				
+
+			
+			return this;
+		}
+
+		this.instantiate = function()
+		{
+			return angular.copy( this );
+		}
+
+		this.toStatePath = function(path)
+		{
+			return path.replace(/\//g, '.').replace(/^\./, '');
+		};
+
+		this.setActive = function(path)
+		{
+			this.iterateCheck(this.menuItems, this.toStatePath(path));
+		};
+
+		this.setActiveParent = function(item)
+		{
+			item.isActive = true;
+			item.isOpen = true;
+
+			if(item.parent)
+				this.setActiveParent(item.parent);
+		};
+
+		this.iterateCheck = function(menuItems, currentState)
+		{
+			angular.forEach(menuItems, function(item)
+			{
+				if(item.state == currentState)
+				{
+					item.isActive = true;
+
+					if(item.parent != null)
+						$menuItemsRef.setActiveParent(item.parent);
+				}
+				else
+				{
+					item.isActive = false;
+					item.isOpen = false;
+
+					if(item.menuItems.length)
+					{
+						$menuItemsRef.iterateCheck(item.menuItems, currentState);
+					}
+				}
+			});
+		}
+	});
