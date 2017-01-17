a12def4a742e8d1c81bdb6b634315339a02c17f4
Mudassir91 <mudassir.hussain@accionlabs.com>
2017-01-17 13:09:04 +0530

    update user

--- src/main/java/com/accion/recruitment/web/controller/UserController.java
+++ src/main/java/com/accion/recruitment/web/controller/UserController.java
@@ -298,6 +298,10 @@ public class UserController {
             }
 
             try{
+                user.setId(oldUser.getId());
+                user.setPassword(oldUser.getPassword());
+                user.setCreatedBy(oldUser.getCreatedBy());
+                user.setCreatedDate(oldUser.getCreatedDate());
                 user.setUpdatedBy(principal.getName());
             }catch (Exception e){
 
@@ -305,11 +309,23 @@ public class UserController {
             user.setUpdatedDate(new Date(sdf.format(currentDate)));
 
             if(user.getRole().equals(oldUser.getRole())){
-                if(this.userService.saveUser(user)){
-                    return new ResponseEntity<String>(new Gson().toJson(UserHttpStatusEnums.USER_UPDATED.ResponseMsg()), HttpStatus.OK);
-                }else {
-                    return new ResponseEntity<String>(new Gson().toJson(UserHttpStatusEnums.USER_NOT_UPDATED.ResponseMsg()), HttpStatus.OK);
+                if(userSkills.getTechnicalScreenerSkills()!=null){
+                    technicalScreenerSkills=userSkills.getTechnicalScreenerSkills();
+                    technicalScreenerSkillsList=this.getTechnicalSkillsObject(technicalScreenerSkills);
+                    user.getTechnicalScreenerDetailsDSkillsSet().addAll(technicalScreenerSkillsList);
+                    if(this.userService.saveUser(user)){
+                        return new ResponseEntity<String>(new Gson().toJson(UserHttpStatusEnums.USER_UPDATED.ResponseMsg()), HttpStatus.OK);
+                    }else {
+                        return new ResponseEntity<String>(new Gson().toJson(UserHttpStatusEnums.USER_NOT_UPDATED.ResponseMsg()), HttpStatus.OK);
+                    }
+                }else{
+                    if(this.userService.saveUser(user)){
+                        return new ResponseEntity<String>(new Gson().toJson(UserHttpStatusEnums.USER_UPDATED.ResponseMsg()), HttpStatus.OK);
+                    }else {
+                        return new ResponseEntity<String>(new Gson().toJson(UserHttpStatusEnums.USER_NOT_UPDATED.ResponseMsg()), HttpStatus.OK);
+                    }
                 }
+
             }else{
                 String deleteUserGroupQuery="delete  FROM default.user_group where userSet_id="+oldUser.getId();
                 if(this.userService.deleteRecordByQuery(deleteUserGroupQuery)){
