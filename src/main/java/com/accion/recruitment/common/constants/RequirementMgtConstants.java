9b910173b9420a24eb3d04813f780378f228f387
MoinGodil <moin.godil@accionlabs>
2017-01-19 15:49:32 +0530

    HOT-236 Backend Stubs Create Req

---
+++ src/main/java/com/accion/recruitment/common/constants/RequirementMgtConstants.java
@@ -0,0 +1,10 @@
+package com.accion.recruitment.common.constants;
+
+/**
+ * Created by AL1028 on 1/19/17.
+ */
+public class RequirementMgtConstants {
+
+    public static final String REQUIREMENT_CREATE = "/hot/requirement/create";
+
+}
--- src/main/java/com/accion/recruitment/web/controller/RequirementMgtController.java
+++ src/main/java/com/accion/recruitment/web/controller/RequirementMgtController.java
@@ -1,7 +1,42 @@
 package com.accion.recruitment.web.controller;
 
+import com.accion.recruitment.common.constants.LoginRestURIConstants;
+import com.accion.recruitment.common.constants.RequirementMgtConstants;
+import com.accion.recruitment.common.enums.ReqtMgtEnums;
+import com.accion.recruitment.common.enums.UserHttpStatusEnums;
+import com.accion.recruitment.service.RequirementMgtService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
- * Created by AL1028 on 1/11/17.
+ * Created by MoinGodil on 1/11/17.
  */
+@Controller
 public class RequirementMgtController {
+
+    @Autowired
+    RequirementMgtService requirementMgtService;
+
+    @ApiOperation(value = "Requirement Created ", httpMethod="POST"
+            , notes = "Creates Position/Requirement")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Requirement Created "),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
+
+    @RequestMapping(value = RequirementMgtConstants.REQUIREMENT_CREATE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<Object> createRequirement() {
+
+        return new ResponseEntity<Object>(ReqtMgtEnums.REQT_CREATED.ResponseMsg(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
